ede762c0592357b5c084f31ccc5b81a907641c75
Anastasia Lipiäinen <anastasia.lipiainen@avarko.com>
2018-10-11 18:39:05 +0300

    added finnish translations

---
+++ data/fi/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'Ei pidä lainkaan paikkansa',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Ei pidä juurikaan paikkansa',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Pitää osittain paikkainsa, osittain ei',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Pitää jokseenkin paikkansa',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Pitää täysin paikkansa',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Ei pidä lainkaan paikkansa',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Ei pidä juurikaan paikkansa',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Pitää osittain paikkainsa, osittain ei',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Pitää jokseenkin paikkansa',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Pitää täysin paikkansa',
+      score: 1,
+      color: 5
+    }
+  ]
+}
