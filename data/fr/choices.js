7b2afaf508b7b2f4331790bcd57e52692eda8f28
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-02-19 15:41:58 +0100

    standard --fix (patch)

--- data/fr/choices.js
+++ data/fr/choices.js
@@ -1,31 +1,31 @@
 module.exports = {
   plus: [
     {
-      //text: 'Very Inaccurate',
+      // text: 'Very Inaccurate',
       text: 'Fortement en désaccord',
       score: 1,
       color: 1
     },
     {
-      //text: 'Moderately Inaccurate',
+      // text: 'Moderately Inaccurate',
       text: 'Plutot en désaccord',
       score: 2,
       color: 2
     },
     {
-      //text: 'Neither Accurate Nor Inaccurate',
+      // text: 'Neither Accurate Nor Inaccurate',
       text: 'Ni en accord ni en désaccord',
       score: 3,
       color: 3
     },
     {
-      //text: 'Moderately Accurate',
+      // text: 'Moderately Accurate',
       text: 'Plutot en accord',
       score: 4,
       color: 4
     },
     {
-      //text: 'Very Accurate',
+      // text: 'Very Accurate',
       text: 'Fortement en accord',
       score: 5,
       color: 5
@@ -33,31 +33,31 @@ module.exports = {
   ],
   minus: [
     {
-      //text: 'Very Inaccurate',
+      // text: 'Very Inaccurate',
       text: 'Fortement en désaccord',
       score: 5,
       color: 1
     },
     {
-      //text: 'Moderately Inaccurate',
+      // text: 'Moderately Inaccurate',
       text: 'Plutot en désaccord',
       score: 4,
       color: 2
     },
     {
-      //text: 'Neither Accurate Nor Inaccurate',
+      // text: 'Neither Accurate Nor Inaccurate',
       text: 'Ni en accord ni en désaccord',
       score: 3,
       color: 3
     },
     {
-      //text: 'Moderately Accurate',
+      // text: 'Moderately Accurate',
       text: 'Plutot en accord',
       score: 2,
       color: 4
     },
     {
-      //text: 'Very Accurate',
+      // text: 'Very Accurate',
       text: 'Fortement en accord',
       score: 1,
       color: 5
