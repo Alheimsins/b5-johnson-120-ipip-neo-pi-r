db031d420883addccf21c8b2608a4441e99bc53d
Punit Singh <thepunitsingh@gmail.com>
2019-04-05 17:51:47 +0530

    Added Hindi

---
+++ data/hi/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'अत्यंत अयथार्थ',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'कुछ अयथार्थ',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'ना तो यथार्थ न ही अयथार्थ',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'कुछ यथार्थ',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'अत्यंत यथार्थ',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'अत्यंत अयथार्थ',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'कुछ अयथार्थ',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'ना तो यथार्थ न ही अयथार्थ',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'कुछ यथार्थ',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'अत्यंत यथार्थ',
+      score: 1,
+      color: 5
+    }
+  ]
+}
