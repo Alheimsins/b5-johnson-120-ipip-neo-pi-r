719522be3d4008d393fa9f131c941b178d3c97b2
Arslan Nasir <arslan@mobylogix.com>
2020-01-24 19:50:45 +0500

    Danish language support has been added

---
+++ data/da/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'Meget upræcist',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Rimelig upræcist',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Hverken præcist eller upræcist',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Rimelig præcist',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Meget præcist',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Meget upræcist',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Rimelig upræcist',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Hverken præcist eller upræcist',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Rimelig præcist',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Meget præcist',
+      score: 1,
+      color: 5
+    }
+  ]
+}
