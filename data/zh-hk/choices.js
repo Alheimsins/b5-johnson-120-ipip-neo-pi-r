dcb66d26a55237ee2314a4052822b8fa1b992e59
theoryspace <ansonann@gmail.com>
2023-03-06 17:26:11 -0800

    Adding translation for zh-hk Chinese (Hong Kong)

---
+++ data/zh-hk/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: '非常不準確',
+      score: 1,
+      color: 1
+    },
+    {
+      text: '不準確',
+      score: 2,
+      color: 2
+    },
+    {
+      text: '既不能說準，也不能說不準',
+      score: 3,
+      color: 3
+    },
+    {
+      text: '準確',
+      score: 4,
+      color: 4
+    },
+    {
+      text: '非常準確',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: '非常不準確',
+      score: 5,
+      color: 1
+    },
+    {
+      text: '不準確',
+      score: 4,
+      color: 2
+    },
+    {
+      text: '既不能說準，也不能說不準',
+      score: 3,
+      color: 3
+    },
+    {
+      text: '準確',
+      score: 2,
+      color: 4
+    },
+    {
+      text: '非常準確',
+      score: 1,
+      color: 5
+    }
+  ]
+}
