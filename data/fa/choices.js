74803bf43d9355b22d806d2d4a38eb7c985f9b7f
Ali Sabet <84999560+hialisabet@users.noreply.github.com>
2023-09-16 22:19:36 +0330

    translate choices to fa

---
+++ data/fa/choices.js
@@ -0,0 +1,66 @@
+module.exports = {
+  plus: [
+    {
+      //text: 'Very Inaccurate',
+      text: 'کاملا مخالفم',
+      score: 1,
+      color: 1
+    },
+    {
+      //text: 'Moderately Inaccurate',
+      text: 'مخالفم',
+      score: 2,
+      color: 2
+    },
+    {
+      //text: 'Neither Accurate Nor Inaccurate',
+      text: 'مطمئن نیستم',
+      score: 3,
+      color: 3
+    },
+    {
+      //text: 'Moderately Accurate',
+      text: 'موافقم',
+      score: 4,
+      color: 4
+    },
+    {
+      //text: 'Very Accurate',
+      text: 'کاملا موافقم',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      //text: 'Very Inaccurate',
+      text: 'کاملا مخالفم',
+      score: 5,
+      color: 1
+    },
+    {
+      //text: 'Moderately Inaccurate',
+      text: 'مخالفم',
+      score: 4,
+      color: 2
+    },
+    {
+      //text: 'Neither Accurate Nor Inaccurate',
+      text: 'مطمئن نیستم',
+      score: 3,
+      color: 3
+    },
+    {
+      //text: 'Moderately Accurate',
+      text: 'موافقم',
+      score: 2,
+      color: 4
+    },
+    {
+      //text: 'Very Accurate',
+      text: 'کاملا موافقم',
+      score: 1,
+      color: 5
+    }
+  ]
+}
