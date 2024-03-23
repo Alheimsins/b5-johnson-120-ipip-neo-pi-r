ce454b9a2b92eb30247a881dc7a5845cb2491301
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-03-24 00:35:49 +0100

    Standard fix

--- data/fa/choices.js
+++ data/fa/choices.js
@@ -1,31 +1,31 @@
 module.exports = {
   plus: [
     {
-      //text: 'Very Inaccurate',
+      // text: 'Very Inaccurate',
       text: 'کاملا مخالفم',
       score: 1,
       color: 1
     },
     {
-      //text: 'Moderately Inaccurate',
+      // text: 'Moderately Inaccurate',
       text: 'مخالفم',
       score: 2,
       color: 2
     },
     {
-      //text: 'Neither Accurate Nor Inaccurate',
+      // text: 'Neither Accurate Nor Inaccurate',
       text: 'مطمئن نیستم',
       score: 3,
       color: 3
     },
     {
-      //text: 'Moderately Accurate',
+      // text: 'Moderately Accurate',
       text: 'موافقم',
       score: 4,
       color: 4
     },
     {
-      //text: 'Very Accurate',
+      // text: 'Very Accurate',
       text: 'کاملا موافقم',
       score: 5,
       color: 5
@@ -33,31 +33,31 @@ module.exports = {
   ],
   minus: [
     {
-      //text: 'Very Inaccurate',
+      // text: 'Very Inaccurate',
       text: 'کاملا مخالفم',
       score: 5,
       color: 1
     },
     {
-      //text: 'Moderately Inaccurate',
+      // text: 'Moderately Inaccurate',
       text: 'مخالفم',
       score: 4,
       color: 2
     },
     {
-      //text: 'Neither Accurate Nor Inaccurate',
+      // text: 'Neither Accurate Nor Inaccurate',
       text: 'مطمئن نیستم',
       score: 3,
       color: 3
     },
     {
-      //text: 'Moderately Accurate',
+      // text: 'Moderately Accurate',
       text: 'موافقم',
       score: 2,
       color: 4
     },
     {
-      //text: 'Very Accurate',
+      // text: 'Very Accurate',
       text: 'کاملا موافقم',
       score: 1,
       color: 5
