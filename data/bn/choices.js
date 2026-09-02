2f03cbcbf172356832434c52063d80e5f07086d1
tanvirmahfuz100 <tanvirmahfuz100@gmail.com>
2026-09-02 12:06:24 +0600

    Add Bengali (bn) translation
    - 120 IPIP-NEO-PI-R items translated to Bengali
    - Choices translated as: খুবই ভুল, মোটামুটি ভুল, ঠিক বা ভুল কোনোটাই নয়, মোটামুটি ঠিক, খুবই ঠিক
    - Priorities: facet-level meaning accuracy over literal translation

---
+++ data/bn/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'খুবই ভুল',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'মোটামুটি ভুল',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'ঠিক বা ভুল কোনোটাই নয়',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'মোটামুটি ঠিক',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'খুবই ঠিক',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'খুবই ভুল',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'মোটামুটি ভুল',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'ঠিক বা ভুল কোনোটাই নয়',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'মোটামুটি ঠিক',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'খুবই ঠিক',
+      score: 1,
+      color: 5
+    }
+  ]
+}
